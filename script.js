c2fc8b8b70f8688976b5cea0e5caff478e02fec5
Calvin Macintosh <calvinmac633@gmail.com>
2019-12-11 01:38:53 -0500

    first commit to test code

--- script.js
+++ script.js
@@ -1,158 +1,325 @@
-// function promptMe() {
-//     var pwdChar = prompt("How many characters would you like your password to be?");
-// }
-// console.log(pwdChar)
-
-var pwdCap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var pwdLow = "abcdefghijklmnopqrstuvwxyz";
-var pwdNum = "0123456789";
-var pwdSpec = "!@#$%^&*()";
-
-var pwdCapArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var pwdLowArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var pwdNumArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var pwdSpecArray = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var pwd = [];
+$("#time").html("Time: 0")
+var timer = 75;
 
 
-//This may or may not be right
-// var randPassword = Array(pwdChar).fill(pwdCap).map(function (x) { return x[Math.floor(Math.random() * x.length)] }).join('');
+$("#startButton").off().on("click", function () {
+    // TIMER START
+    var timerInterval = setInterval(function () {
+        timer--;
+        $("#time").html("Time: " + timer)
 
-// Capture amount of characters the person wants
-// I did up above
-
-// Brings up prompt and stores password character value
-//NEED TO ADD MINIMUM LIMIT
-
-function promptMe() {
-    //Trying to get the box to clear when button is hit again
-    var pwd = [];
-    document.getElementById("password").innerHTML = [];
-
-    var pwdChar = prompt("How many characters would you like your password to be?");
-    console.log(pwdChar);
-    if (pwdChar < 8 || pwdChar > 128) {
-        alert("Password must be between 8 and 128 characters");
-        return
-    } else if (isNaN(pwdChar)) {
-        alert("Enter in a number between 8 and 128");
-        return
-    }
-
-
-    // Confirms
-    var includeSpec = confirm("Click 'Okay' to include special characters");
-    var includeNum = confirm("Click 'Okay' to include numbers");
-    var includeCap = confirm("Click 'Okay' to include upper case letters");
-    var includeLow = confirm("Click 'Okay' to include lower case letters");
-
-    // FUNCTIONS
-    function one(a) {
-        for (var i = 0; i < pwdChar; i++) {
-            pwd.push(a[Math.floor(Math.random() * a.length)]);
-            // for the end in the line above, can you say random() * a.length??
-            document.getElementById("password").innerHTML = pwd.join("");
+        if (timer === 0) {
+            clearInterval(timerInterval);
         }
+    }, 1000);
+
+    // QUESTION #1
+
+
+    //Clear content and print options
+    function printBtn() {
+        $("#headings").html("Which of these is not a programming language?")
+        var options = ["a. HTML", "b. javascript", "c. Porygon", "d. CSS"];
+        $("#main").empty();
+        $("#startButton").empty();
+        var main = $("#main-section")
+        // $.each(options, function (index, element) {
+        //     var answersList = $("<ul>")
+        //     main.append(answersList)
+        //     answersList.append("<button id=answers>" + element + "</button>")
+
+        // for (i = 0; i < options.length; i++) {
+        //     var answersList = $("<ul>")
+        //     main.append(answersList)
+        //     var answers = $("<button class=answers>" + options[i] + "</button>").attr("id", [i])
+        //     answersList.append(answers)
+        // }
+
+        options.forEach(function (element, index) {
+            var answersList = $("<ul>")
+            main.append(answersList)
+            var answers = $("<button class=answers>" + element + "</button>").attr("id", "firstQ" + index)
+            answersList.append(answers)
+        })
     }
-    function two(a, b) {
-        for (var i = 0; i < pwdChar; i++) {
-            pwd.push(a[Math.floor(Math.random() * a.length)]);
-            pwd.push(b[Math.floor(Math.random() * b.length)]);
-            document.getElementById("password").innerHTML = pwd.join("").slice(pwdChar);
+    printBtn();
+
+    /************************************************ */
+    // Click response to first set of questions
+    $("#firstQ0").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+
+        $("#headings").html("What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?")
+        var options = ["a. Repeater", "b. Loop", "c. Debugger", "d. Clone"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            options.forEach(function (element, index) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                var answers = $("<button class=answers>" + element + "</button>").attr("id", "secondQ" + index)
+                answersList.append(answers)
+            })
         }
-    }
+        printBtn();
 
-    function three(a, b, c) {
-        for (var i = 0; i < pwdChar; i++) {
-            pwd.push(a[Math.floor(Math.random() * a.length)]);
-            pwd.push(b[Math.floor(Math.random() * b.length)]);
-            pwd.push(c[Math.floor(Math.random() * c.length)]);
-            document.getElementById("password").innerHTML = pwd.join("").slice(pwdChar).slice(pwdChar);
+    })
+
+    $("#firstQ1").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+
+        $("#headings").html("What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?")
+        var options = ["a. Repeater", "b. Loop", "c. Debugger", "d. Clone"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            options.forEach(function (element, index) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                var answers = $("<button class=answers>" + element + "</button>").attr("id", "secondQ" + index)
+                answersList.append(answers)
+            })
         }
-    }
-    function four(a, b, c, d) {
-        for (var i = 0; i < pwdChar; i++) {
-            pwd.push(a[Math.floor(Math.random() * a.length)]);
-            pwd.push(b[Math.floor(Math.random() * b.length)]);
-            pwd.push(c[Math.floor(Math.random() * c.length)]);
-            pwd.push(d[Math.floor(Math.random() * d.length)]);
-            document.getElementById("password").innerHTML = pwd.join("").slice(pwdChar).slice(pwdChar).slice(pwdChar);
+        printBtn();
+    })
+
+    $("#firstQ2").off().on("click", function () {
+        event.preventDefault();
+
+        $("#headings").html("What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?")
+        var options = ["a. Repeater", "b. Loop", "c. Debugger", "d. Clone"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            options.forEach(function (element, index) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                var answers = $("<button class=answers>" + element + "</button>").attr("id", "secondQ" + index)
+                answersList.append(answers)
+            })
         }
-    }
+        printBtn();
+    })
 
-    // Start from all 5 = TRUE --> to only 1 being TRUE
-    // Will most likely need to incorporate splicing into the "multiple" scenarios
+    $("#firstQ3").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
 
-    if (includeSpec && includeNum && includeCap && includeLow) {
-        four(pwdSpecArray, pwdNumArray, pwdCapArray, pwdLowArray);
-    }
-    else if (includeSpec && includeNum && includeCap) {
-        three(pwdSpecArray, pwdNumArray, pwdCapArray);
-    }
-    else if (includeSpec && includeNum && includeLow) {
-        three(pwdSpecArray, pwdNumArray, pwdLowArray);
-    }
-    else if (includeSpec && includeCap && includeLow) {
-        three(pwdSpecArray, pwdCapArray, pwdLowArray);
-    }
-    else if (includeNum && includeCap && includeLow) {
-        three(pwdNumArray, pwdCapArray, pwdLowArray);
-    }
-    else if (includeSpec && includeNum) {
-        two(pwdSpecArray, pwdNumArray);
-    }
-    else if (includeSpec && includeCap) {
-        two(pwdSpecArray, pwdCapArray);
-    }
-    else if (includeSpec && includeLow) {
-        two(pwdSpecArray, pwdLowArray);
-    }
-    else if (includeNum && includeLow) {
-        two(pwdNumArray, pwdLowArray);
-    }
-    else if (includeCap && includeLow) {
-        two(pwdCapArray, pwdLowArray);
-    }
-    else if (includeNum && includeCap) {
-        two(pwdNumArray, pwdCapArray);
-    }
-    else if (includeSpec) {
-        one(pwdSpecArray);
-    }
-    else if (includeNum) {
-        one(pwdNumArray);
-    }
-    else if (includeCap) {
-        one(pwdCapArray);
-    }
-    else if (includeLow) {
-        one(pwdLowArray);
-    }
-}
+        clearInterval(timerInterval);
+        $("#headings").html("All done! You're final score is " + timer)
+        $("#main-section").empty();
+        var main = $("#main-section");
+        var enterInitials = $("<p> Enter your initials here: </p>")
+        enterInitials.html("Enter your initials: ")
+        var initials = document.createElement("input")
+        initials.setAttribute("type", "text")
+        main.append(enterInitials)
+        enterInitials.append(initials)
+        var submitScore = $("<button class=score> Submit </button>")
+        enterInitials.append(submitScore)
 
-function copy() {
-    var copyText = document.getElementById("password");
-    copyText.select();
-    copyText.setSelectionRange(0, 9999);
-    document.execCommand("copy");
-    alert("Your password has been copied to your clipboard!");
-}
 
-    // function one (a) {
-    //     for (var i = 0; i < pwdChar; i++) {
-    //     pwd.push(a[Math.floor(Math.random() * pwdChar)]);
-    //     document.getElementById("password").innerHTML = pwd.join("");
-    // }
+        // $("#headings").html("What is the element called that can continue to execute a block of code as long as the specified condition remains TRUE?")
+        // var options = ["a. Repeater", "b. Loop", "c. Debugger", "d. Clone"];
 
-        // ****TAKE ADVANTAGE OF "PUSH" method in arrays -> to tack on to end of an array
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+        // //Clear content and print options
+        // function printBtn() {
+        //     $("#main-section").empty();
+        //     $("#startButton").empty();
+        //     var main = $("#main-section")
+        //     options.forEach(function (element, index) {
+        //         var answersList = $("<ul>")
+        //         main.append(answersList)
+        //         var answers = $("<button class=answers>" + element + "</button>").attr("id", "secondQ" + index)
+        //         answersList.append(answers)
+        //     })
+        // }
+        // printBtn();
+    })
 
-        // **** Also take advantage of Merging Arrays
-        // https://www.w3schools.com/js/js_array_methods.asp
 
-// "Click Okay to include using special characters"
-// These will be confirms, not prompts
 
-//Copy to clipboard
-// ****** This is a bonus, but should still try to do it
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /************************************************ */
+    // Click response to second set of questions
+    $("#secondQ0").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+
+        $("#headings").html("Bootstrap is to CSS as jQuery is to...?")
+        var options = ["a. HTML", "b. event listeners", "c. javascript", "d. API"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            options.forEach(function (element, index) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                var answers = $("<button class=answers>" + element + "</button>").attr("id", "thirdQ" + index)
+                answersList.append(answers)
+            })
+        }
+        printBtn();
+    })
+
+    $("#secondQ1").off().on("click", function () {
+        event.preventDefault();
+
+
+        $("#headings").html("Bootstrap is to CSS as jQuery is to...?")
+        var options = ["a. HTML", "b. event listeners", "c. javascript", "d. API"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            $.each(options, function (index, element) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                answersList.append("<button class=answers>" + element + "</button>").attr("id", "thirdQ" + index)
+            })
+        }
+        printBtn();
+    })
+
+    $("#secondQ2").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+
+        $("#headings").html("Bootstrap is to CSS as jQuery is to...?")
+        var options = ["a. HTML", "b. event listeners", "c. javascript", "d. API"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            $.each(options, function (index, element) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                answersList.append("<button class=answers>" + element + "</button>").attr("id", "thirdQ" + index)
+            })
+        }
+        printBtn();
+    })
+
+    $("#secondQ3").off().on("click", function () {
+        event.preventDefault();
+        alert("Wrong! Minus 10 seconds!")
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+        timer--;
+
+        $("#headings").html("Bootstrap is to CSS as jQuery is to...?")
+        var options = ["a. HTML", "b. event listeners", "c. javascript", "d. API"];
+
+        //Clear content and print options
+        function printBtn() {
+            $("#main-section").empty();
+            $("#startButton").empty();
+            var main = $("#main-section")
+            $.each(options, function (index, element) {
+                var answersList = $("<ul>")
+                main.append(answersList)
+                answersList.append("<button class=answers>" + element + "</button>").attr("id", "thirdQ" + index)
+            })
+        }
+        printBtn();
+    })
+
+})
+
+
+
+
+
+    // EVERYTHING GOES ABOVE THIS LINE (maybe not for storage)
+// })
+
+//In order for the questions to work, for example. all of the "secQi" click events need to go INSIDE all of the firstQi events
 
